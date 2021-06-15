64c0d0ad282aa7a9b458ba363bc3be3f327048ec
moom0o <48740106+moom0o@users.noreply.github.com>
2021-06-15 18:09:57 -0400

    Increase max retries for internet archive

--- youtube-dl/upload.sh
+++ youtube-dl/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #-H x-archive-keep-old-version:0
-ia upload $1 "$2" --metadata="collection:$1" --metadata="mediatype:movies" --retries 10
+ia upload $1 "$2" --metadata="collection:$1" --metadata="mediatype:movies" --retries 100000
 rm "$2"
