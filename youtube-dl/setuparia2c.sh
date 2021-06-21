657b368759884bb42ef5bf067017387c88effa36
moom0o <48740106+moom0o@users.noreply.github.com>
2021-06-21 09:22:58 -0400

    Create setuparia2c.sh

---
+++ youtube-dl/setuparia2c.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/moom0o/aria2.git
+cd aria2
+apt install nettle-dev libgmp-dev libssh2-1-dev libc-ares-dev libxml2-dev zlib1g-dev libsqlite3-dev pkg-config libssl-dev libxml2-dev libcppunit-dev autoconf automake autotools-dev autopoint libtool
+autoreconf -i
+./configure
+make
+cp ./src/aria2c /usr/bin/
