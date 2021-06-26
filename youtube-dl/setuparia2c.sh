20072d80219b393a905d7619c41c800a342a4d8b
moom0o <48740106+moom0o@users.noreply.github.com>
2021-06-25 20:26:48 -0400

    headless confirmation

--- youtube-dl/setuparia2c.sh
+++ youtube-dl/setuparia2c.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/moom0o/aria2.git
 cd aria2
-apt install nettle-dev libgmp-dev libssh2-1-dev libc-ares-dev libxml2-dev zlib1g-dev libsqlite3-dev pkg-config libssl-dev libxml2-dev libcppunit-dev autoconf automake autotools-dev autopoint libtool
+apt install nettle-dev libgmp-dev libssh2-1-dev libc-ares-dev libxml2-dev zlib1g-dev libsqlite3-dev pkg-config libssl-dev libxml2-dev libcppunit-dev autoconf automake autotools-dev autopoint libtool -y
 autoreconf -i
 ./configure
 make
