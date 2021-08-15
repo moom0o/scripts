d79c9a50187e6a3c68059713d4647894f1f96947
moom0o <48740106+moom0o@users.noreply.github.com>
2021-08-14 23:59:29 -0400

    Update upload.sh

--- youtube-dl/upload.sh
+++ youtube-dl/upload.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 #-H x-archive-keep-old-version:0
 ia upload $1 "$2" --metadata="collection:$1" --metadata="mediatype:movies" --retries 100000
+# Comments - ia upload $1 "$2" "$3" --metadata="collection:$1" --metadata="mediatype:movies" --retries 100000
 rm "$2"
+# Comments - rm "$3"
