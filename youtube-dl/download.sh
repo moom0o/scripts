3b693024a330650ff25185a2f857b254c7a70f14
moom0o <48740106+moom0o@users.noreply.github.com>
2021-12-23 16:37:48 -0500

    Update download.sh

--- youtube-dl/download.sh
+++ youtube-dl/download.sh
@@ -1,2 +1,3 @@
-yt-dlp --cookies 'cookies.txt' --download-archive "archive.log" -i --add-metadata --all-subs --embed-subs --embed-thumbnail -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4" -o '%(title)s.%(ext)s' "$1" --exec "bash upload.sh $2 %(filepath)q"
-#Comments - yt-dlp --cookies 'cookies.txt' --download-archive "archive.log" -i --write-comments --add-metadata --all-subs --embed-subs --embed-thumbnail -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4" -o '%(title)s.%(ext)s' "$1" --exec "bash upload.sh $2 %(filepath)q %(__infojson_filename)q"
+#youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' "$*" --exec "bash upload.sh" -o '%(title)s.%(ext)s' --write-thumbnail
+yt-dlp --force-ipv4 --cookies 'cookies.txt' --download-archive "archive.log" -i --add-metadata --write-info-json --all-subs --embed-subs --embed-thumbnail -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4" -o '%(title)s - %(id)s.%(ext)s' "$1" --exec "bash upload.sh $2 %(filepath)q %(__infojson_filename)q"
+#--reject-title "\bLive\b"
