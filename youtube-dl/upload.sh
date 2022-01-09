0590a627c7446608303e914f7ce2cb88fcad2d23
moom0o <48740106+moom0o@users.noreply.github.com>
2022-01-09 00:06:28 -0500

    upload livechat

--- youtube-dl/upload.sh
+++ youtube-dl/upload.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 #-H x-archive-keep-old-version:0
-ia upload $1 "$2" "$3" --metadata="collection:$1" --metadata="mediatype:movies" --retries 1000000
-rm "$2" "$3"
+if [ "$4" == "NA" ]; then
+    ia upload $1 "$2" "$3" --metadata="collection:$1" --metadata="mediatype:movies" --retries 1000000
+    rm "$2" "$3"
+else
+    ia upload $1 "$2" "$3" "$4" --metadata="collection:$1" --metadata="mediatype:movies" --retries 1000000
+    rm "$2" "$3" "$4"
+fi
